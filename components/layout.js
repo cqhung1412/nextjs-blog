9a8e835563b4f8bce7eb6022e99e5e5e0bb2767c
Hùng Bear <hungcq3@vng.com.vn>
2022-12-14 16:04:02 +0700

    base template

---
+++ components/layout.js
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import AppHead from "./app-head";
+
+const name = "Big City Bear";
+export const siteTitle = "Next.js Sampling";
+
+const ProfileImage = ({ height, width }) => {
+  return (
+    <Image
+      priority
+      src="/images/profile.jpg"
+      className={utilStyles.borderCircle}
+      height={height}
+      width={width}
+      alt="profile image"
+    />
+  );
+};
+
+const Layout = ({ children, home }) => {
+  let header = (
+    <>
+      <ProfileImage height={144} width={144} />
+      <h1 className={utilStyles.heading2Xl}>{name}</h1>
+    </>
+  );
+  if (!home)
+    header = (
+      <>
+        <Link href="/">
+          <ProfileImage height={108} width={108} />
+        </Link>
+        <h2 className={utilStyles.headingLg}>
+          <Link href="/" className={utilStyles.colorInherit}>
+            {name}
+          </Link>
+        </h2>
+      </>
+    );
+  return (
+    <div className={styles.container}>
+      <AppHead siteTitle={siteTitle} />
+      <header className={styles.header}>{header}</header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
